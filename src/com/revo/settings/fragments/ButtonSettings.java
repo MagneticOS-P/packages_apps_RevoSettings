0fd6165b5aa18d78bd1dd944890aa58a056293e2
nelo360 <nelo360@gmail.com>
2018-09-25 09:14:31 +0200

    add missing constant

--- src/com/revo/settings/fragments/ButtonSettings.java
+++ src/com/revo/settings/fragments/ButtonSettings.java
@@ -57,7 +57,7 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
 
     @Override
     public int getMetricsCategory() {
-        return MetricsProto.MetricsEvent.;
+        return MetricsProto.MetricsEvent.REVO_SETTINGS;
     }
 
 }
