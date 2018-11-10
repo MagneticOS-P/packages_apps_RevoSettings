Havoc-OS/android_packages_apps_HavocSettings

Add edge gestures (2/2)

src/com/havoc/settings/preferences/SecureSettingIntListPreference.java
@@ -1,0 +1,47 @@
+/* 
+ * Copyright (C) 2018 Havoc-OS
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License. 
+ */ 
+ 
+package com.havoc.settings.preferences; 
+ 
+import android.content.Context; 
+import android.util.AttributeSet; 
+ 
+public class SecureSettingIntListPreference extends SecureSettingListPreference { 
+ 
+    public SecureSettingIntListPreference(Context context, AttributeSet attrs, int defStyle) { 
+        super(context, attrs, defStyle); 
+    } 
+ 
+    public SecureSettingIntListPreference(Context context, AttributeSet attrs) { 
+        super(context, attrs); 
+    } 
+ 
+    public SecureSettingIntListPreference(Context context) { 
+        super(context); 
+    } 
+ 
+    @Override 
+    protected boolean persistString(String value) { 
+        return persistInt(Integer.parseInt(value)); 
+    } 
+ 
+    @Override 
+    protected String getPersistedString(String defaultReturnValue) { 
+        return String.valueOf(getPersistedInt(Integer.parseInt(defaultReturnValue))); 
+    } 
+ 
+} 
+ 

src/com/havoc/settings/preferences/SecureSettingSeekBarPreference.java
@@ -1,0 +1,38 @@
+/*
+ * Copyright (C) 2017 AICP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.havoc.settings.preferences;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+public class SecureSettingSeekBarPreference extends SeekBarPreferenceCham {
+
+    public SecureSettingSeekBarPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setPreferenceDataStore(new SecureSettingsStore(context.getContentResolver()));
+    }
+
+    public SecureSettingSeekBarPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setPreferenceDataStore(new SecureSettingsStore(context.getContentResolver()));
+    }
+
+    public SecureSettingSeekBarPreference(Context context) {
+        super(context, null);
+        setPreferenceDataStore(new SecureSettingsStore(context.getContentResolver()));
+    }
+}

MagneticOS-P/packages_apps_RevoSettings

src/com/revo/settings/preferences/SecureSettingIntListPreference.java
@@ -1,0 +1,47 @@
+/* 
+ * Copyright (C) 2018 Havoc-OS
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License. 
+ */ 
+ 
+package com.revo.settings.preferences; 
+ 
+import android.content.Context; 
+import android.util.AttributeSet; 
+ 
+public class SecureSettingIntListPreference extends SecureSettingListPreference { 
+ 
+    public SecureSettingIntListPreference(Context context, AttributeSet attrs, int defStyle) { 
+        super(context, attrs, defStyle); 
+    } 
+ 
+    public SecureSettingIntListPreference(Context context, AttributeSet attrs) { 
+        super(context, attrs); 
+    } 
+ 
+    public SecureSettingIntListPreference(Context context) { 
+        super(context); 
+    } 
+ 
+    @Override 
+    protected boolean persistString(String value) { 
+        return persistInt(Integer.parseInt(value)); 
+    } 
+ 
+    @Override 
+    protected String getPersistedString(String defaultReturnValue) { 
+        return String.valueOf(getPersistedInt(Integer.parseInt(defaultReturnValue))); 
+    } 
+ 
+} 
+ 

src/com/revo/settings/preferences/SecureSettingSeekBarPreference.java
@@ -1,0 +1,38 @@
+/*
+ * Copyright (C) 2017 AICP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.revo.settings.preferences;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+public class SecureSettingSeekBarPreference extends SeekBarPreferenceCham {
+
+    public SecureSettingSeekBarPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setPreferenceDataStore(new SecureSettingsStore(context.getContentResolver()));
+    }
+
+    public SecureSettingSeekBarPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setPreferenceDataStore(new SecureSettingsStore(context.getContentResolver()));
+    }
+
+    public SecureSettingSeekBarPreference(Context context) {
+        super(context, null);
+        setPreferenceDataStore(new SecureSettingsStore(context.getContentResolver()));
+    }
+}